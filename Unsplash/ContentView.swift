60e1843dbd026b276ce991f42656de50c97b4d95
SangJin Han <s202188@naver.com>
2022-01-26 20:57:31 +0900

    chore: 신규 프로젝트 생성

---
+++ Unsplash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unsplash
+//
+//  Created by 한상진 on 2022/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by 한상진 on 2022/01/26.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
